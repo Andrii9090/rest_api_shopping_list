d302464b88f18abc9a39c78b227068015f8caeb9
Andrii9090 <desandrey95@gmail.com>
2024-04-18 23:14:05 +0200

    change user router

--- src/routers/list.router.ts
+++ src/routers/list.router.ts
@@ -6,11 +6,11 @@ const router = Router()
 router.use(isAutentificate)
 
 router.post('/', listController.create.bind(listController))
+router.get('/', listController.getAll.bind(listController)) // ?skip=0&take=10 fisrt how many objects skiped and second how many obj should be returnet. It's for pagination
 router.post('/:id/add-user', listController.addUserToList.bind(listController))
 router.get('/:id/clear', listController.clear.bind(listController))
 router.put('/:id', listController.update.bind(listController))
 router.delete('/:id', listController.delete.bind(listController))
-router.get('/', listController.getAll.bind(listController)) // ?skip=0&take=10 fisrt how many objects skiped and second how many obj should be returnet. It's for pagination
 
 
 export default router 
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -6,10 +6,10 @@ const router = Router()
 
 const controller = UserController
 
-router.post('/login', controller.login.bind(controller))
-router.use(isAutentificate).get('/access-code', controller.generateAccessCode.bind(controller))
 router.post('/', controller.createUser.bind(controller))
 router.use(isAutentificate).get('/', controller.getUserData.bind(controller))
 router.use(isAutentificate).put('/', controller.update.bind(controller))
+router.post('/login', controller.login.bind(controller))
+router.use(isAutentificate).get('/access-code', controller.generateAccessCode.bind(controller))
 
 export default router

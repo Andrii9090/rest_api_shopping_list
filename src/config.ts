1941ce13ab9ac97346dcdb22b97150e71cb535ba
Andrii9090 <desandrey95@gmail.com>
2024-04-20 22:15:55 +0200

    add IMAGE_PATH to .env

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export default {
     production: true,
     port: process.env['PORT'] ? Number(process.env['PORT']) : 3000,
     baseUrl: 'https://liston.ovh/api',
-    imagePath: path.resolve(process.cwd(), 'uploads', 'images'),
+    imagePath: process.env.IMAGE_PATH || path.resolve(process.cwd(), 'uploads', 'images'),
 }
 
  

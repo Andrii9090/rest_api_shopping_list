792c19dc8c635fa7e7921ba0aae44e0a7f0c849f
Andrii9090 <desandrey95@gmail.com>
2024-04-21 10:28:26 +0200

    change config process.env

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export default {
     production: true,
     port: process.env['PORT'] ? Number(process.env['PORT']) : 3000,
     baseUrl: 'https://liston.ovh/api',
-    imagePath: process.env.IMAGE_PATH,
+    imagePath: process.env.IMAGE_PATH || path.resolve(process.cwd(), 'uploads', 'images'),
 }
 
  
